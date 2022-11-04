4ed368c5c10a695b1d5ae13fc6cd45a27406683f
Chris <chrishethertonweb@gmail.com>
2022-11-04 17:00:27 +0100

    refactored Card and added props

--- src/components/Card.js
+++ src/components/Card.js
@@ -9,7 +9,7 @@ function Card(props) {
         <div className="card">
             <img className='card-image' src={props.img} alt="" />
             <div className="card-tag">
-                <p><img className='star' src={star} alt="" />{props.rating}<span>({props.reviewCount}) USA </span></p>
+                <p><img className='star' src={star} alt="" /> {props.rating} <span> ({props.reviewCount}) &middot; {props.country} </span></p>
                 <p className="card-tag__title">{props.title}</p>
                 <p className="card-tag__price"><strong>From {props.price}$ </strong>/ person</p>
         </div>   
