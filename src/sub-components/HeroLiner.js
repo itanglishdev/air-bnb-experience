5cc6a93a22a6d1c31d22bd00d27f4835676880cf
Chris <chrishethertonweb@gmail.com>
2022-11-04 14:45:56 +0100

    syntax error correction on Hero

--- src/sub-components/HeroLiner.js
+++ src/sub-components/HeroLiner.js
@@ -1,7 +1,7 @@
 function HeroLiner() {
     return ( 
         <div className="hero">
-            <h1 className="title">Online Experience</h1>
+            <h1 className="title">Online Experiences</h1>
             <p className="title__sub-paragraph">Join unique interactive activities led by one-of-a-kind hosts-all without leaving home.</p>
         </div>
      );
